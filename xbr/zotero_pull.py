0c813ed60bfeb03e085cf66a65f283c7fe954058
nicomo <nicolas.morin@gmail.com>
2014-07-04 13:28:39 -0400

    handling of credentials for zotero

--- xbr/zotero_pull.py
+++ xbr/zotero_pull.py
@@ -1,7 +1,17 @@
 from pyzotero import zotero
 import json
+from pprint import pprint
 
-zot = zotero.Zotero('123401','user','jPSjb41TyrIXK18hGkHdBXv5')
+with open('../static/data/credentials.json') as credentials:
+	data = json.load(credentials)
+	zot_id = data["zot_id"]
+	zot_coll_xbr = data["zot_coll_xbr"]
+
+pprint(data)
+pprint(zot_id)
+pprint(zot_coll_xbr)
+#
+zot = zotero.Zotero(zot_id, 'user', zot_coll_xbr)
 items = zot.items()
 with open('../static/data/xbr_biblio.json', 'w') as bibliofile:
 	json.dump(items, bibliofile)
