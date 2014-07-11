5eb1f59777c02a551e8bae5b20af1209a449113b
nicomo <nicolas.morin@gmail.com>
2014-07-11 14:15:52 -0400

    insert bibliography into xbr page

--- static/javascripts/xbr_biblio.js
+++ static/javascripts/xbr_biblio.js
@@ -2,93 +2,189 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
     $.getJSON( "../static/data/xbr_biblio.json", function(bib_data) {
-      
-      console.log( "success" );
-      console.log(bib_data.length);
 
       var html = '';
 
-      for (var i = 0; i < bib_data.length; i++) {
 
-        html+= '<li>';
-
-        // authors
-        if (typeof bib_data[i].creators != 'undefined') {
-          for (var j = 0; j < bib_data[i].creators.length; j++) {
-            if (bib_data[i].creators[j].creatorType == 'author' || bib_data[i].creators[j].creatorType == 'translator') {
-              if (typeof bib_data[i].creators[j].lastName != 'undefined') {
-                html += bib_data[i].creators[j].firstName + ' ' + bib_data[i].creators[j].lastName;
-              }
-            }
-            if (bib_data[i].creators[j].creatorType == 'translator') {
-              html += " (trans.)";
-            }
-            if (j < bib_data[i].creators.length -1) {
-              html += " and ";
-            } else {
-              html+= ", ";
-            }
-          }
-        }
-        
-        // title
-        if (typeof bib_data[i].title != 'undefined') {
-          html += "<span class='title'>" + bib_data[i].title + "</span>";
-        }
-
-
-        // TODO remove, testing itemtypes
-        // html += " itemtype is " + bib_data[i].itemType + " ";
-        
-        // if book, we have a publisher
-        if (bib_data[i].itemType == 'book') {
-          if (typeof bib_data[i].publisher != 'undefined') {
-            html += " (" + bib_data[i].publisher + ", ";
-          }
-        } else if (bib_data[i].itemType == 'thesis') { // thesis, university in place of publisher
-          if (typeof bib_data[i].university != 'undefined') {
-            html += " (" + bib_data[i].university + ", ";
-          }
-        } else if (bib_data[i].itemType == 'journalArticle') { // oh man, this is an article :-/
-          html += " in ";
-          if (typeof bib_data[i].publicationTitle != 'undefined') {
-            html += bib_data[i].publicationTitle;
-          }
-          if (typeof bib_data[i].volume != 'undefined') {
-            html += " - vol. " + bib_data[i].volume;
-          }
-          if (typeof bib_data[i].issue != 'undefined') {
-            html += " - issue " + bib_data[i].issue + ", ";
-          }
-
-
-
-
-
-
-
-          
-
-
-
-
-
-
-
-
-        }
-        
-        if (typeof bib_data[i].date != 'undefined') {
-          html += " " + bib_data[i].date + ")";
-        }
-
-        html += '</li>';
+      // First create the array of keys so that we can sort:
+      var sort_array = [];
+      for (var key in bib_data) {
+        // 1st key is main contributor, either author, interviewer, etc.
+        var person;
+        if (bib_data[key].hasOwnProperty('author')) {
+          person = bib_data[key].author[0].family;
+        } else if (bib_data[key].hasOwnProperty('interviewer')) {
+          person = bib_data[key].interviewer[0].family;
+        };
+        // 2d key is date, always present...
+        sort_array.push({key:key,author:person,pub_year:bib_data[key].issued['date-parts'][0][0]});
       }
 
+      // Now sort:
+      sort_array.sort(function(a,b){
+        var a1 = a.author.toLowerCase();
+        var a2 = b.author.toLowerCase();
+
+        var d1 = a.pub_year;
+        var d2 = b.pub_year;
+
+        if (a1 != a2) {
+          if (a1 < a2) return -1;
+          if (a1 > a2) return 1;
+          return 0;
+        }
+        if (d1 < d2) return -1;
+        if (d1 > d2) return 1;
+        return 0;
+      });
+
+      // Now process that object:
+      for (var i=0;i<sort_array.length;i++) {
+
+        var item = bib_data[sort_array[i].key];
+        
+        // TODO Remove console.log
+        console.log(item.type);
+        html += "<li>";
+        if (item.hasOwnProperty('author')) {
+          for (var j = 0; j < item.author.length; j++) {
+            html += item.author[j].given + " " + item.author[j].family + ". ";
+          }
+          if (item.hasOwnProperty('translator')) {
+            for (var j = 0; j < item.translator.length; j++) {
+              html += item.translator[j].given + " " + item.translator[j].family + " (trans. ";
+            }
+            if (item.hasOwnProperty('language')) {
+              html += "- " + item.language;
+            }
+            html+= "). ";
+          };
+        } else if (item.hasOwnProperty('interviewer')) {
+          for (var j = 0; j < item.interviewer.length; j++) {
+            html += item.interviewer[j].given + " " + item.interviewer[j].family + ". ";
+          }
+        }
+
+        // title
+        if (item.type === "book") {
+          if (item.hasOwnProperty('title')) {
+            html += "<span class=title>" + item.title + ".</span>";
+          };
+        } else if (item.type === "chapter" || item.type === 'article-magazine' || item.type === 'article-newspaper' || item.type === 'article-journal') {
+          if (item.hasOwnProperty('title')) {
+            html += "\"" + item.title + "\" in ";
+          };
+          
+          html += "<span class=title>" + item["container-title"] + ".</span>";  
+          if (item.hasOwnProperty('container-author')) {
+            for (var j = 0; j < item['container-author'].length; j++) {
+              html += " " + item['container-author'][j].given + " " + item['container-author'][j].family + ", ed. ";
+            }
+          }
+          if (item.hasOwnProperty('editor')) {
+            html += " " + item['editor'][j].given + " " + item['editor'][j].family + ", ed. ";
+          }
+          if (item.hasOwnProperty('volume')) {
+            html += " Vol. "+ item.volume; 
+          };
+          if (item.hasOwnProperty('issue')) {
+            html += " Issue " + item.issue + " ";
+          };
+
+
+
+
+
+
+        } else if (item.type === "thesis") {
+          if (item.hasOwnProperty('title')) {
+            html += "\"" + item.title + "\" ";
+          };
+        } else if (item.type === "webpage" || item.type === 'interview') {
+          html += "<span class=title>" + item.title + ".</span>";
+        }
+
+        // publisher
+        html+= " (";
+        if (item.type === "thesis") {
+          html += "PhD diss., ";
+        };
+        if (item.hasOwnProperty('publisher-place')) {
+          html += item["publisher-place"] + ": "
+        };
+        if (item.hasOwnProperty('publisher')) {
+          html += item.publisher + ", ";
+        };
+
+        // date
+        if (item.issued["date-parts"][0] != 'undefined') {
+          for (var j = 0; j < item.issued["date-parts"][0].length; j++) {
+            if (j === 1) {
+              console.log("in");
+              switch(item.issued["date-parts"][0][1]) {
+                case 1:
+                  html += " Jan. "
+                  break;
+                case 2:
+                  html += " Feb. "
+                  break;
+                case 3:
+                  html += " Mar. "
+                  break;
+                case 4:
+                  html += " Apr. "
+                  break;
+                case 5:
+                  html += " May "
+                  break;
+                case 6:
+                  html += " Jun. "
+                  break;
+                case 7:
+                  html += " Jul. "
+                  break;
+                case 8:
+                  html += " Aug. "
+                  break;
+                case 9:
+                  html += " Sep. "
+                  break;
+                case 10:
+                  html += " Oct. "
+                  break;
+                case 11:
+                  html += " Nov. "
+                  break;
+                case 12:
+                  html += " Jan. "
+                  break;
+                default:
+                  html += item.issued["date-parts"][0][1];
+              }
+            } else {
+              html += item.issued["date-parts"][0][j];
+            };
+            if  (j < item.issued["date-parts"][0].length -1) {
+              html += " ";
+            } else {
+              html += ")";
+            }
+          }
+        };
+
+        if (item.hasOwnProperty('page')) {
+          html += " " + item.page + ". ";
+        };
+        // DOI or URL
+        if (item.hasOwnProperty('DOI')) {
+          html += " doi: <a href='http://dx.doi.org/" + item.DOI + "'>" + item.DOI + "</a>";
+        } else if (item.hasOwnProperty('URL')) {
+          html += " <a href='" + item.URL + "'>" + item.URL + "</a>";
+        }  
+      }
+      
+      // and... print
       $('#xbr_biblio').html(html);
 
     })
-
-    
-
 });
--- xbr/zotero_pull.py
+++ xbr/zotero_pull.py
@@ -1,18 +1,37 @@
+#
+# using the zotero API to retrieve items
+# WORK IN PROGRESS - NOT FUNCTIONAL
+#
+
+
+
+
 from pyzotero import zotero
 import json
-from pprint import pprint
 
+# we get the credentials to access the zotero API
 with open('../static/data/credentials.json') as credentials:
 	data = json.load(credentials)
 	zot_id = data["zot_id"]
 	zot_coll_xbr = data["zot_coll_xbr"]
 
-pprint(data)
-pprint(zot_id)
-pprint(zot_coll_xbr)
-#
+# connect
 zot = zotero.Zotero(zot_id, 'user', zot_coll_xbr)
-items = zot.items()
-with open('../static/data/xbr_biblio.json', 'w') as bibliofile:
-	json.dump(items, bibliofile)
 
+# add parameters
+zot.add_parameters(order='creator')
+
+# get all references in library
+zot.num_items()
+items0 = zot.items() # 1st batch, default limit is 50
+lazy = zot.iterfollow()
+items1 = lazy.next() #2d batch
+
+
+# write to json file
+with open('../static/data/xbr_biblio0.json', 'w') as bibliofile:
+	json.dump(items0, bibliofile)
+
+# write to json file
+with open('../static/data/xbr_biblio1.json', 'w') as bibliofile:
+	json.dump(items1, bibliofile)
