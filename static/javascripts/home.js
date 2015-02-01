aa6650a7af1653b723b7665e3eeb5bf86b9ed843
nicomo <nicolas.morin@gmail.com>
2015-02-01 15:10:40 -0400

    ajout marche Magadan dans menu home

--- static/javascripts/home.js
+++ static/javascripts/home.js
@@ -6,5 +6,9 @@ $( document ).ready(function() {
 	$("#marches_menu_1").on('click touch', function () {
 		$.cookie("nicomo_marches_part_id", 1, { path: '/marches', expires: 20 });		
 		$.cookie('nicomo_marches_step_id', 0, { path: '/marches', expires: 20 });
+	});
+	$("#marches_menu_2").on('click touch', function () {
+		$.cookie("nicomo_marches_part_id", 2, { path: '/marches', expires: 20 });		
+		$.cookie('nicomo_marches_step_id', 0, { path: '/marches', expires: 20 });
 	});	
 });
