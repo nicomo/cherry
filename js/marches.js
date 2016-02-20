26b1b4f63d691f98eefe178c5210540574348bcb
nicomo <nicolas.morin@gmail.com>
2016-02-20 15:36:11 -0400

    rough draft new version of site using material design lite

---
+++ js/marches.js
@@ -0,0 +1,123 @@
+$( document ).ready(function() {
+
+  // let's set the id and cookie for this page's content
+  var part_id = document.getElementById('marche_id').value;
+  var step_id;
+  var data_length;
+  var liste_marches = ["Charleville-Mézières", "Lisbonne", "Magadan", "Paris", "New York", "Saipan", "Manaus"];
+  var liste_marcheurs = ["Jean", "João", "Jan", "Joel", "Jane", "Jaufré", "Jefferson"];
+
+  // we use a cookie to know at which step we're at in this particular walk
+  if (Cookies.get('nicomo_marches_step_id')) {
+    step_id = Cookies.get('nicomo_marches_step_id');
+  } else {
+    step_id = 0;   
+    Cookies.set('nicomo_marches_step_id', step_id, { expires: 20, path: '' });
+  };
+
+  // init page content for 1st time
+  src_initialize(part_id, step_id);
+
+});
+
+
+
+function src_initialize(my_part_id, my_step_id) {
+
+  //
+  // TODO READ JSON JUST ONCE RATHER THAN EACH TIME AROUND
+  //
+  var json_file = 'marches_' + my_part_id +'.json';
+  console.log(json_file);
+  // read our data in json file
+  $.getJSON(json_file, function(data) {
+
+    data_length = data.length;
+    
+    // set background image
+    var img_url = '../assets/images/marches_' + my_part_id + '_' + my_step_id + '.png';
+
+    $('#marches-content').css({
+      'background': 'url("' + img_url + '") no-repeat center center fixed',
+      '-webkit-background-size': 'cover',
+      '-moz-background-size': 'cover',
+      '-o-background-size': 'cover',
+      'background-size': 'cover'
+    });
+
+    // TITLE
+    // name of character
+    $('#marcheur').html(liste_marcheurs[my_part_id]);
+    // city
+    $('#marche').html(liste_marches[my_part_id]);
+    // kilometer 
+    var km = data[my_step_id].km;
+    $('#km').html(km);
+
+    // TEXT
+    var my_txt = '<p>' + data[my_step_id].txt + '</p>';
+    $('#marches_txt').html(my_txt);
+
+    // MAPS
+    var lat = parseFloat(data[my_step_id].LatLng.Lat);
+    var lng = parseFloat(data[my_step_id].LatLng.Lng);
+    console.log(lat + " " + lng + " (types: " + (typeof lat) + ", " + (typeof lng) + ")");
+    initMap(lat, lng);
+
+    // NEXT
+
+    // if at the end of this particular walk
+
+
+
+
+
+//TODO 
+// si on est au bout : url bouton marche_ part_id +1 + changer l'intitulé du bouton "Nom, Lieu" avec part_id +1
+// si on n'est pas au bout: bouton "avancer" -> step_id += 1.my_step_id
+
+
+
+
+    if (my_step_id >= data.length -1) {
+
+      // we're at the end of this walk: move on to next walk, step 0
+      my_part_id++;
+      Cookies.set('nicomo_marches_step_id', 0, { expires: 20, path: '' });
+      // we change the text of the button and its url
+      var avancer_txt = liste_marcheurs[my_part_id] + ', ' + liste_marches[my_part_id];
+      console.log(avancer_txt);
+      var next_url = 'http://www.nicolasmorin.com/marches/marche_' + my_part_id + '.html';
+      $("#avancer").text(avancer_txt).attr('href', next_url);
+
+    } else {
+      my_step_id++;
+      console.log(my_step_id);
+      Cookies.set('nicomo_marches_step_id', my_step_id, { expires: 20, path: '' });
+      
+    }
+  
+
+
+
+  });
+}
+
+// google map
+function initMap(my_lat, my_lng) {
+  var fenway = {lat: my_lat, lng: my_lng};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: fenway,
+    //TODO : fix InvalidValueError: setCenter: not a LatLng or LatLngLiteral: in property lat: not a number 
+    zoom: 14
+  });
+  var panorama = new google.maps.StreetViewPanorama(
+      document.getElementById('pano'), {
+        position: fenway,
+        pov: {
+          heading: 34,
+          pitch: 10
+        }
+      });
+  map.setStreetView(panorama);
+}
---
+++ js/xbr_biblio.js
@@ -0,0 +1,182 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    $.getJSON( "../assets/json/xbr_biblio.json", function(bib_data) {
+
+      var html = '';
+
+
+      // First create the array of keys so that we can sort:
+      var sort_array = [];
+      for (var key in bib_data) {
+        // 1st key is main contributor, either author, interviewer, etc.
+        var person;
+        if (bib_data[key].hasOwnProperty('author')) {
+          person = bib_data[key].author[0].family;
+        } else if (bib_data[key].hasOwnProperty('interviewer')) {
+          person = bib_data[key].interviewer[0].family;
+        };
+        // 2d key is date, always present...
+        sort_array.push({key:key,author:person,pub_year:bib_data[key].issued['date-parts'][0][0]});
+      }
+
+      // Now sort:
+      sort_array.sort(function(a,b){
+        var a1 = a.author.toLowerCase();
+        var a2 = b.author.toLowerCase();
+
+        var d1 = a.pub_year;
+        var d2 = b.pub_year;
+
+        if (a1 != a2) {
+          if (a1 < a2) return -1;
+          if (a1 > a2) return 1;
+          return 0;
+        }
+        if (d1 < d2) return -1;
+        if (d1 > d2) return 1;
+        return 0;
+      });
+
+      // Now process that object:
+      for (var i=0;i<sort_array.length;i++) {
+
+        var item = bib_data[sort_array[i].key];
+        
+        // TODO Remove console.log
+        console.log(item.type);
+        html += "<li class='mdl-list__item'>";
+        if (item.hasOwnProperty('author')) {
+          for (var j = 0; j < item.author.length; j++) {
+            html += item.author[j].given + " " + item.author[j].family + ". ";
+          }
+          if (item.hasOwnProperty('translator')) {
+            for (var j = 0; j < item.translator.length; j++) {
+              html += item.translator[j].given + " " + item.translator[j].family + " (trans. ";
+            }
+            if (item.hasOwnProperty('language')) {
+              html += "- " + item.language;
+            }
+            html+= "). ";
+          };
+        } else if (item.hasOwnProperty('interviewer')) {
+          for (var j = 0; j < item.interviewer.length; j++) {
+            html += item.interviewer[j].given + " " + item.interviewer[j].family + ". ";
+          }
+        }
+
+        // title
+        if (item.type === "book") {
+          if (item.hasOwnProperty('title')) {
+            html += "&nbsp;" + item.title + ".";
+          };
+        } else if (item.type === "chapter" || item.type === 'article-magazine' || item.type === 'article-newspaper' || item.type === 'article-journal') {
+          if (item.hasOwnProperty('title')) {
+            html += "&ldquo;" + item.title + "&rdquo; in ";
+          };
+          
+          html += item["container-title"] + ".";  
+          if (item.hasOwnProperty('container-author')) {
+            for (var j = 0; j < item['container-author'].length; j++) {
+              html += " " + item['container-author'][j].given + " " + item['container-author'][j].family + ", ed. ";
+            }
+          }
+          if (item.hasOwnProperty('editor')) {
+            for (var j = 0; j < item.editor.length; j++) {
+              html += " " + item.editor[j].given + " " + item.editor[j].family + ", ed. ";              
+            }
+          }
+          if (item.hasOwnProperty('volume')) {
+            html += " Vol. "+ item.volume; 
+          };
+          if (item.hasOwnProperty('issue')) {
+            html += " Issue " + item.issue + " ";
+          };
+
+        } else if (item.type === "thesis") {
+          if (item.hasOwnProperty('title')) {
+            html += "\"" + item.title + "\" ";
+          };
+        } else if (item.type === "webpage" || item.type === 'interview') {
+          html += "<span class=xbr_title>" + item.title + ".</span>";
+        }
+
+        // publisher
+        html+= " (";
+        if (item.type === "thesis") {
+          html += "PhD diss., ";
+        };
+        if (item.hasOwnProperty('publisher-place')) {
+          html += item["publisher-place"] + ": "
+        };
+        if (item.hasOwnProperty('publisher')) {
+          html += item.publisher + ", ";
+        };
+
+        // date
+        if (item.issued["date-parts"][0] != 'undefined') {
+          for (var j = 0; j < item.issued["date-parts"][0].length; j++) {
+            if (j === 1) {
+              console.log("in");
+              switch(item.issued["date-parts"][0][1]) {
+                case 1:
+                  html += " Jan. "
+                  break;
+                case 2:
+                  html += " Feb. "
+                  break;
+                case 3:
+                  html += " Mar. "
+                  break;
+                case 4:
+                  html += " Apr. "
+                  break;
+                case 5:
+                  html += " May "
+                  break;
+                case 6:
+                  html += " Jun. "
+                  break;
+                case 7:
+                  html += " Jul. "
+                  break;
+                case 8:
+                  html += " Aug. "
+                  break;
+                case 9:
+                  html += " Sep. "
+                  break;
+                case 10:
+                  html += " Oct. "
+                  break;
+                case 11:
+                  html += " Nov. "
+                  break;
+                case 12:
+                  html += " Jan. "
+                  break;
+                default:
+                  html += item.issued["date-parts"][0][1];
+              }
+            } else {
+              html += item.issued["date-parts"][0][j];
+            };
+            if  (j < item.issued["date-parts"][0].length -1) {
+              html += " ";
+            } else {
+              html += ")";
+            }
+          }
+        };
+
+        if (item.hasOwnProperty('page')) {
+          html += " " + item.page + ". ";
+        };
+
+      }
+      
+      // and... print
+      $('#xbr_biblio').html(html);
+
+    })
+});
