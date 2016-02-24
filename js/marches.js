0017fdc95c9a3a755e669c19a831d16ccf3fba8c
nicomo <nicolas.morin@gmail.com>
2016-02-24 12:18:55 -0400

    revert

--- js/marches.js
+++ js/marches.js
@@ -2,11 +2,16 @@ $( document ).ready(function() {
 
   // let's set the id and cookie for this page's content
   var part_id = document.getElementById('marche_id').value;
-  var step_id = 0;
+  var step_id;
   var data_length;
 
   // we use a cookie to know at which step we're at in this particular walk
-  /*
+  
+// TODO: si on a un cookie qui a un step 12 pour la marche 2, par ex.
+// et qu'on clique sur le lien direct vers marche 0, par ex.
+// on va se retrouver sur le step 12 de la marche 0. 
+// => ajouter le suivi des marches dans le cookie
+
   if (Cookies.get('nicomo_marches_step_id')) {
     step_id = Cookies.get('nicomo_marches_step_id');
   } else {
@@ -14,8 +19,6 @@ $( document ).ready(function() {
     Cookies.set('nicomo_marches_step_id', step_id, { expires: 20, path: '' });
   };
 
-
-*/
   // hide/show map
   $( "#map_hideshow" ).click(function() {
     if ($( ".gmap-content" ).css( 'visibility' ) == 'visible') {
@@ -25,7 +28,6 @@ $( document ).ready(function() {
       $( ".gmap-content" ).css( 'visibility', 'visible');
       $("#map_hideshow").text("Masquer la carte");
     }
-
   });
 
   // init page content for 1st time
@@ -95,14 +97,14 @@ function src_initialize(my_part_id, my_step_id) {
         console.log(avancer_txt);
         var next_url = 'http://www.nicolasmorin.com/marches/marche_' + my_part_id + '.html';
         $("#avancer").text(avancer_txt).attr('href', next_url);
-        // Cookies.remove('nicomo_marches_step_id');
+        Cookies.remove('nicomo_marches_step_id');
       }
     } else {
       // we stay in this walk, next step
       $('#avancer').on('touchstart click', function(){
         my_step_id++;
         console.log('next-else, my_step_id: ' + my_step_id);
-        // Cookies.set('nicomo_marches_step_id', my_step_id, { expires: 20, path: '' });
+        Cookies.set('nicomo_marches_step_id', my_step_id, { expires: 20, path: '' });
         src_initialize(my_part_id, my_step_id);  
       });
     
