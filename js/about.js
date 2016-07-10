26ea53c2d0746048eab3a6f4a4206331bc721ba2
nicomo <nicolas.morin@gmail.com>
2016-07-10 10:19:06 -0400

    cleaner js trigger for cards details

--- js/about.js
+++ js/about.js
@@ -1,9 +1,27 @@
-function showdetails(detailid) {
-	$('.about-detail').each(function(index){
-		if ($(this).attr("id") === detailid) {
-			$(this).show(400)
-		} else {
-			$(this).hide()
-		}
-	});
-}
+$(document).ready(function() {
+
+
+
+
+	if (window.matchMedia("(min-width: 1024px)").matches) {
+	  /* the viewport is at least 1024 pixels wide */
+  	$('.showdetails').on("click", function() {
+			detailid = $(this).attr("id")
+			$('.about-detail').each(function(index){
+				if ($(this).attr("id") === detailid) {
+					$(this).show(400)
+				} else {
+					$(this).hide()
+				}
+			});
+		});
+
+
+
+
+	} else {
+	  /* the viewport is less than 1024 pixels wide */
+	  
+	}
+	
+});
